b13f3eabf339e7fa6866b8f223b41ca3603686c1
BlackWolf4455 <90320042+BlackWolf4455@users.noreply.github.com>
2021-12-28 10:42:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int min = 100; int pos = 0;
+            int[] mas = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                mas[i] = rnd.Next(100);
+                if(mas[i] < min)
+                {
+                    min = mas[i];
+                    pos = 1;
+                    {
+                        Console.WriteLine("{0}", mas[i]);
+                    }
+                    Console.WriteLine("Наим{0}", min);
+                    (mas[8], mas[pos]) = (mas[pos], mas[9]);
+                    for (int j = 0; j < 10; j++)
+                    {
+                         Console.WriteLine("{0}", mas[i]);
+                    }
+                }
+            }
+        }
+    }
